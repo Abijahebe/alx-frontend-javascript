1b1955029a4a91571adf9f75f874f0093152d623
Abijah Ebe <Abijahebe911@gmail.com>
2023-09-25 12:58:39 +0000

    task js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -8,7 +8,7 @@ export function getLast() {
 }
 
 export funtion taskNext() {
-	var combination = 'But sometimes let';
+	let combination = 'But sometimes let';
 	combination += getLast();
 
 	return combination;
