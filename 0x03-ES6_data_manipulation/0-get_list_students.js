09cd9270efb9d95a791fd21cdc1965632acbf41f
Abijah Ebe <Abijahebe911@gmail.com>
2023-10-02 17:47:54 +0000

    task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+export default function getListStudents() {
+	return [
+		{
+			id: 1,
+			firstName: 'Guillaume',
+			location: 'San francisco',
+		},
+		{
+			id: 2,
+			firstName: 'James',
+			location: 'Columbia',
+		},
+		{
+			id: 5,
+			firstName: 'Serena',
+			location: 'San francisco',
+		},
+	];
+}
